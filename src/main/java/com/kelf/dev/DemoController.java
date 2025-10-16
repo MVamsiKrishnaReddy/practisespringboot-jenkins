15509d2d28e82e9a6d159fc243b53cb27373fa1d
MVK <2300031063@kluniversity.in>
2025-10-16 08:02:06 +0530

    Updated

--- src/main/java/com/kelf/dev/DemoController.java
+++ src/main/java/com/kelf/dev/DemoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,9 +25,21 @@ public class DemoController {
 		return Arrays.asList(data);
 	}
 	
+	@GetMapping("/posts/{id}")
+    public Object getPostById(@PathVariable int id) {
+        Object data = restTemplate.getForObject("https://jsonplaceholder.typicode.com/posts/" + id, Object.class);
+        return data;
+    }
+	
 	@GetMapping("/comments")
 	public List<Object> todos() {
 		Object[] data=restTemplate.getForObject("https://jsonplaceholder.typicode.com/comments", Object[].class);
 		return Arrays.asList(data);
 	}
+	
+	@GetMapping("/comments/{id}")
+    public Object getCommentById(@PathVariable int id) {
+        Object data = restTemplate.getForObject("https://jsonplaceholder.typicode.com/comments/" + id, Object.class);
+        return data;
+    }
 }
