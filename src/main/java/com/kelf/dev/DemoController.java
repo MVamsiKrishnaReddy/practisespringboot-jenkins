faaf08e46b929a57ca497b8364c9a6850fd7770a
MVK <2300031063@kluniversity.in>
2025-10-16 07:56:18 +0530

    Updated

--- src/main/java/com/kelf/dev/DemoController.java
+++ src/main/java/com/kelf/dev/DemoController.java
@@ -1,12 +1,32 @@
 package com.kelf.dev;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DemoController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
 	@GetMapping("/")
-	public String login() {
-		return "Logged in successfully";
+	public String jenkins() {
+		return "jenkins is working successfully";
+	}
+	
+	@GetMapping("/posts")
+	public List<Object> userdata() {
+		Object[] data=restTemplate.getForObject("https://jsonplaceholder.typicode.com/posts", Object[].class);
+		return Arrays.asList(data);
+	}
+	
+	@GetMapping("/comments")
+	public List<Object> todos() {
+		Object[] data=restTemplate.getForObject("https://jsonplaceholder.typicode.com/comments", Object[].class);
+		return Arrays.asList(data);
 	}
 }
