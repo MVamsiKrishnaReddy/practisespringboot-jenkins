99d7d70b8736c5aab78d79ad2c0d19292be08067
MVK <2300031063@kluniversity.in>
2025-10-16 07:57:42 +0530

    Updated

--- src/main/java/com/kelf/dev/JenkinsBackEndApplication.java
+++ src/main/java/com/kelf/dev/JenkinsBackEndApplication.java
@@ -3,6 +3,8 @@ package com.kelf.dev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class JenkinsBackEndApplication extends SpringBootServletInitializer {
@@ -11,5 +13,9 @@ public class JenkinsBackEndApplication extends SpringBootServletInitializer {
 		SpringApplication.run(JenkinsBackEndApplication.class, args);
 		System.out.println("Project running");
 	}
-
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
